90e4564e741c74ed914d3a31eb3bd858e24f841b
berk <abdulberk@gmail.com>
2023-03-28 02:11:52 +0300

    ui all done

--- components/content/index.tsx
+++ components/content/index.tsx
@@ -3,20 +3,20 @@ import style from './style.module.scss'
 import Card from '../card'
 import { CardProps } from '../card';
 import { CardNames } from '../card';
+import Table from '../table'
 
 function Content() {
 
 
   const cards = [
-    { image: "/courses.svg", sticks: "/courses-sticks.svg", type: "courses", count: 18, title: "Total Courses" },
-    { image: "/views.svg", sticks: "views-sticks.svg", type: "views", count: 1597, title:"Total Views" },
-    { image: "/members.svg", sticks: "members-sticks.svg", type: "members", count: 79, title :"Total Members" },
-  ];
-
-  const cardList = cards.map((card, index) => (
-    <Card key={index} image={card.image} sticks={card.sticks} type={card.type as CardProps["type"]} count={card.count}  title={card.title} />
-  ));
+    { image: "/courses.svg", sticks: "/courses-sticks.svg", type: "courses", count: 18, title: "Courses" },
+    { image: "/views.svg", sticks: "views-sticks.svg", type: "views", count: 1597, title:"Views" },
+    { image: "/members.svg", sticks: "members-sticks.svg", type: "members", count: 79, title :"Members" },
+  ] as CardProps[]
   
+  const cardList = cards.map((card, index) => (
+    <Card key={index} image={card.image} sticks={card.sticks} type={card.type} count={card.count} title={card.title} />
+  ));
 
   return (
     <div className = {style.content}>
@@ -38,6 +38,9 @@ function Content() {
 
       {cardList}
       </div>
+
+      <Table />
+      
 
 
     </div>
--- components/table/index.tsx
+++ components/table/index.tsx
@@ -2,11 +2,102 @@ import React from 'react'
 import style from './style.module.scss'
 
 function Table() {
+
+
+  const courses = [
+    { title: "Course 1", duration: "1h 30m", date: "12/12/2020", price: "$100", author: "John Doeeeeee", status: "rejected" },
+    { title: "Course 2", duration: "1h 30m", date: "12/12/2020", price: "$100", author: "John Doe", status: "rejected" },
+    { title: "Course 3", duration: "1h 30m", date: "12/12/2020", price: "$100", author: "John Doe", status: "pending" },
+    { title: "Course 4", duration: "1h 30m", date: "12/12/2020", price: "$100", author: "John Doe", status: "approved" },
+    { title: "Course 5", duration: "1h 30m", date: "12/12/2020", price: "$100", author: "John Doe", status: "approved" },
+  ]
+
+
+
   return (
-    <div className = {style.table}>
-        <h1>table</h1>
+    <div className={style.table}>
+      <div className={style.table__container}>
+        <div className={style.titleRowContainer}>
+          <div className={style.leftSide}></div>
+
+          <div className={style.rightSide}>
+            <div className={style.title}>Course Management</div>
+
+            <div className={style.buttonContainer}>
+              <span>See all</span>
+              <img src="/arrowright2.svg" width={24} height={24} alt="none" />
+            </div>
+          </div>
+        </div>
+
+        <div className={style.tableHeader}>
+          {["Title", "Duration", "Date", "Price", "Author", "Status"].map(
+            (item, index) => {
+              return (
+                <div className={style.tableHeader__item} key={index}>
+                  {item}
+                </div>
+              );
+            }
+          )}
+        </div>
+
+        <span className={style.horizontalDivider}></span>
+
+        <div className={style.tableBody}>
+          <div className={style.tableBody__row}>
+
+               
+             <div className = {style.coursePic} >
+              <img src="/react.png" width={48} height={48} alt="none" />
+              
+              </div>
+ 
+
+
+            <div className={style.tableBody__row__item}>
+              <span>{courses[0].title}</span>
+            </div>
+
+            <div className={style.tableBody__row__item}>
+              <span>{courses[0].duration}</span>
+            </div>
+
+            <div className={style.tableBody__row__item}>
+              <span>{courses[0].date}</span>
+            </div>
+
+            <div className={style.tableBody__row__item}>
+              <span>{courses[0].price}</span>
+            </div>
+
+            <div className={style.tableBody__row__item}>
+              <span>{courses[0].author}</span>
+            </div>
+
+            <div className={style.tableBody__row__item}>
+            <img src="/rejected.svg" width={16} height={16} alt="none" />
+              <span>
+               
+                {courses[0].status}
+                </span>
+
+
+            </div>
+
+            <div className={style.buttonsContainer}>
+                  <img src="/edit.svg" width={32} height={32} alt="none" />
+                  <img src="/delete.svg" width={32} height={32} alt="none" />
+                </div>
+            
+
+
+
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Table
